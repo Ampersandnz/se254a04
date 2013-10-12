fbee07d7d3cd1ddc61e1b1bc7eedfc856c1af85c
Michael Lo <nz.ampersand@gmail.com>
2013-10-12 15:34:44 +1300

    Initial commit. Counter class added.

---
+++ src/a04/Counter.java
@@ -0,0 +1,19 @@
+package a04;
+
+public class Counter {
+	
+	public int c;
+	
+	public void increment() {
+		c++; 
+	}
+	
+	public void decrement() { 
+		c--; 
+	}
+	
+	public void reset() { 
+		c = 0; 
+	}
+	
+}
